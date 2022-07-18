3388374671cf3e693a1a0a740b4ec17e164a1d27
Luka Miljković <92792328+K0milj@users.noreply.github.com>
2022-07-18 23:50:34 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import { useState, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faX } from '@fortawesome/free-solid-svg-icons'
 import { db } from "./firebase-config";
-import { collection, addDoc, serverTimestamp} from "firebase/firestore";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import TodoList from "./mui_components/TodoList";
 import Button from '@mui/material/Button';
-import firebase from 'firebase/compat/app';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Divider } from "@mui/material";
 
 function App() {
   const [input, setInput] = useState('');
   const [input2, setInput2] = useState('');
   const [style, setStyle] = useState("addTodoWrapper");
+  const [importance, setImportance] = React.useState('');
+
+  const handleImportance = (event) => {
+    setImportance(event.target.value);
+  };
 
   const todosRef = collection(db, "todos");
 
@@ -27,31 +37,29 @@ function App() {
     setInput2('');
   };
 
-  const addTodo = async() => {
+  const addTodo = async () => {
     var date = new Date(input2);
     var dd = date.getDate();
 
-    var mm = date.getMonth()+1; 
+    var mm = date.getMonth() + 1;
     var y = date.getFullYear();
 
     var h = date.getHours();
     var min = date.getMinutes();
 
-    if(dd < 10) 
-    {
-        dd = '0' + dd;
-    } 
-
-    if(mm < 10) 
-    {
-        mm = '0' + mm;
+    if (dd < 10) {
+      dd = '0' + dd;
     }
-    
-    if(h < 10){
+
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+
+    if (h < 10) {
       h = '0' + h;
     }
 
-    if(min < 10){
+    if (min < 10) {
       min = '0' + min;
     }
     date = mm + '-' + dd + '-' + y + " " + h + ":" + min;
@@ -60,11 +68,14 @@ function App() {
       text: input,
       dueDate: date,
       comments: [],
-      timestamp: serverTimestamp()
+      timestamp: serverTimestamp(),
+      importance: importance
     });
     setInput('');
     window.location.reload();
   }
+
+
 
   return (
     <>
@@ -72,12 +83,27 @@ function App() {
       <button id="addBtn" onClick={showAddTodo}><FontAwesomeIcon icon={faPlus}></FontAwesomeIcon></button>
       <div className={style} id="addTodoWrapper">
         <div className="addTodo">
-          <FontAwesomeIcon icon={faX} onClick={hideAddTodo} className="hideTodoBtn" style={{color: "black"}}></FontAwesomeIcon>
+          <FontAwesomeIcon icon={faX} onClick={hideAddTodo} className="hideTodoBtn" style={{ color: "black" }}></FontAwesomeIcon>
           <div>
-            <input type="text" placeholder="Enter todo:" value={input} onChange={event => setInput(event.target.value)}/>
+            <input type="text" placeholder="Enter todo:" value={input} onChange={event => setInput(event.target.value)} />
             <br />
-            <input type="datetime-local" value={input2} onChange={event => setInput2(event.target.value)}/>
-            <Button sx={{margin: '10px 0'}} variant="outlined" onClick={addTodo} disabled={!input || !input2}>Add Todo</Button>
+            <input type="datetime-local" value={input2} onChange={event => setInput2(event.target.value)} />
+            <Box sx={{ minWidth: 120, marginTop: 1 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Importance</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={importance}
+                  label="Importance"
+                  onChange={handleImportance}
+                >
+                  <MenuItem value={"Primary"}>Primary</MenuItem>
+                  <MenuItem value={"Secondary"}>Secondary</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+            <Button sx={{ margin: '10px 0' }} variant="outlined" onClick={addTodo} disabled={!input || !input2}>Add Todo</Button>
           </div>
         </div>
       </div>
--- src/mui_components/TodoList.js
+++ src/mui_components/TodoList.js
@@ -16,6 +16,14 @@ import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import AddCommentIcon from '@mui/icons-material/AddCommentOutlined';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import EditIcon from '@mui/icons-material/Edit';
+import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import Badge from "@mui/material/Badge";
+
+import todopic from "../img/undraw_completing_re_i7ap.svg"
 
 export default function TodoList() {
     const [newTodos, setNewTodos] = useState([]);
@@ -26,127 +34,181 @@ export default function TodoList() {
         width: '100%',
         maxWidth: 360,
         bgcolor: 'background.paper',
-      };
-  
+    };
+
     const todosRef = collection(db, "todos");
     const finishedTodosRef = collection(db, "finishedTodos");
 
     useEffect(() => {
-      const getTodos = async() => {
-        const q = query(todosRef, orderBy("timestamp", "desc"));
-        const data = await getDocs(q);
-        setNewTodos(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-      }
-      getTodos();
+        const getTodos = async () => {
+            const q = query(todosRef, orderBy("timestamp", "desc"));
+            const data = await getDocs(q);
+            setNewTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        }
+        getTodos();
     }, [])
 
     useEffect(() => {
-        const getFinishedTodos = async() => {
-            const q = query(todosRef, orderBy("timestamp", "desc"));    
+        const getFinishedTodos = async () => {
+            const q = query(todosRef, orderBy("timestamp", "desc"));
             const data = await getDocs(q);
-            setFinishedTodos(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+            setFinishedTodos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
         }
         getFinishedTodos();
-      }, [])
-  
-    const deleteTodo = async(id) => {
-      const todoDoc = doc(db, "todos", id);
-      await deleteDoc(todoDoc);
-      window.location.reload();
+    }, [])
+
+    const deleteTodo = async (id) => {
+        const todoDoc = doc(db, "todos", id);
+        await deleteDoc(todoDoc);
+        window.location.reload();
     }
 
-    const finishTodo = async(id) => {
+    const finishTodo = async (id) => {
         const todoDoc = doc(db, "todos", id);
         const docSnap = await getDoc(todoDoc);
         let todoText = docSnap.data().text;
         await deleteDoc(todoDoc);
         await addDoc(finishedTodosRef, {
             text: todoText
-          });
+        });
         window.location.reload();
     }
 
-    const deleteFinishedTodo = async(id) => {
+    const deleteFinishedTodo = async (id) => {
         const finisedTodoDoc = doc(db, "finishedTodos", id);
         await deleteDoc(finisedTodoDoc);
         window.location.reload();
     }
 
-    const AddComment = async(id, newComment) => {  
+    const AddComment = async (id, newComment) => {
         const todoDoc = doc(db, "todos", id);
-        await updateDoc(todoDoc, {
-            "comments": arrayUnion(newComment)
-        });
-        window.location.reload();
+        if (newComment == "") {
+            setOpen(true);
+        } else {
+            await updateDoc(todoDoc, {
+                "comments": arrayUnion(newComment)
+            });
+            window.location.reload();
+        }
     }
 
-    if(newTodos.length == 0){
-        return(
-            <p>No new todos</p>
-        )
-    }else{
+    const [open, setOpen] = React.useState(false);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
+
+    const openModal = () => {
+        <>
+            <h1>Hello</h1>
+        </>
+    }
+
+    const action = (
+        <React.Fragment>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
+
+    if (newTodos.length == 0) {
         return (
-            <div>
+            <div className="no-todos">
+                <p>You are all done!</p>
+                <img src={todopic} alt="todopic" />
+            </div>
+        )
+    } else {
+        return (
+            <div style={{ position: "relative", overflow: "hidden" }}>
                 <List
-                sx={{
-                    width: '100%',
-                    maxWidth: 360,
-                    bgcolor: 'background.paper',
-                }}
-                > 
+                    sx={{
+                        width: '100%',
+                        maxWidth: 360,
+                        bgcolor: 'background.paper',
+                    }}
+                >
                     {newTodos.map((todo) => (
                         <div key={todo.id}>
-                        <Accordion sx={{
+                            <Accordion sx={{
                                 margin: "10px 0"
                             }}>
-                            <AccordionSummary
-                            expandIcon={<MoreHorizIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                            >
-                            <Typography>{todo.text}</Typography>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <Typography variant="body2">
-                                    Due until: {todo.dueDate}
-                                </Typography>
-                                <Accordion style={{
-                                margin: "10px 0"
-                                }}>
-                                    <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
+                                <AccordionSummary
+                                    expandIcon={<MoreHorizIcon />}
                                     aria-controls="panel1a-content"
-                                    id="panel1a-header" 
-                                    >
-                                    <Typography variant="caption">Comments:</Typography>
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                    <List sx={style}>
-                                        {todo.comments.map(comment => {
-                                            return(
-                                                <Box key={comment}>
-                                                    <ListItem>
-                                                    <Typography variant="caption">{comment}</Typography>
-                                                    </ListItem>
-                                                    <Divider />
+                                    id="panel1a-header"
+                                >
+                                    <Typography style={{ color: todo.importance == "Primary" ? "#ff3333" : "black" }}>{todo.text}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography variant="body2">
+                                        Due until: {todo.dueDate}
+                                    </Typography>
+                                    <Typography variant="body2">Importance: {todo.importance}</Typography>
+                                    <Accordion style={{
+                                        margin: "10px 0"
+                                    }}>
+                                        <AccordionSummary
+                                            expandIcon={<ExpandMoreIcon />}
+                                            aria-controls="panel1a-content"
+                                            id="panel1a-header"
+                                        >
+                                            <Badge badgeContent={todo.comments.length} color="primary">
+                                                <ModeCommentIcon style={{ fontSize: "1.3rem" }} color="action" />
+                                            </Badge>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <List sx={style}>
+                                                {todo.comments.map(comment => {
+                                                    return (
+                                                        <Box key={comment}>
+                                                            <ListItem>
+                                                                <Typography variant="caption">{comment}</Typography>
+                                                            </ListItem>
+                                                            <Divider />
+                                                        </Box>
+                                                    )
+                                                })}
+                                                <Box sx={{ marginTop: "10px", display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                                                    <TextField
+                                                        id="standard-basic"
+                                                        placeholder="Enter comment:"
+                                                        variant="standard"
+                                                        value={inputComment}
+                                                        onChange={event => setInputComment(event.target.value)}
+                                                    />
+                                                    <AddCommentIcon disabled={!inputComment} type="submit" className="icon" sx={{ alignSelf: "flex-end", fontSize: "1.3rem" }} onClick={() => { AddComment(todo.id, inputComment) }} />
                                                 </Box>
-                                            )
-                                        })}
-                                        <Box sx={{ marginTop: "10px", display: "flex", alignItems: "center", justifyContent: "space-between"}}>
-                                            <TextField id="standard-basic" placeholder="Enter comment:" variant="standard" value={inputComment} onChange={event => setInputComment(event.target.value)}/>
-                                            <AddCommentIcon type="submit" className="icon" sx={{alignSelf: "flex-end", fontSize: "1.3rem"}} onClick={() => {AddComment(todo.id, inputComment)}}/>
-                                        </Box>
-                                        </List>
-                                    </AccordionDetails>
-                                </Accordion>
-                                <DeleteOutlineIcon className="icon" onClick={() => deleteTodo(todo.id)}/>
-                            </AccordionDetails>
-                        </Accordion>
-                        <Divider />
-                    </div>
+                                            </List>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                    <DeleteOutlineIcon className="icon" onClick={() => deleteTodo(todo.id)} />
+                                    <EditIcon sx={{ marginLeft: "10px" }} className="icon" onClick={() => openModal()} />
+                                </AccordionDetails>
+                            </Accordion>
+                            <Divider />
+                        </div>
                     ))}
                 </List>
+                <div>
+                    <Snackbar
+                        open={open}
+                        autoHideDuration={3000}
+                        onClose={handleClose}
+                        message="Can't enter an empty comment!"
+                        action={action}
+                    />
+                </div>
             </div>
         )
     }
-  }
+}
